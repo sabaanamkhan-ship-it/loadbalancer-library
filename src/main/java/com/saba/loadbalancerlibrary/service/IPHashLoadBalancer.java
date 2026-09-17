0472bdd0362a4d8592842326aba3cd6badd7d92e
Saba Anam <sabaanamkhan@gmail.com>
2026-09-17 16:32:46 +0530

    Phase 2 : add skeleton classess for all load balancing algorithms
    Added empty implementation of load balancer interface : RoundrobinLoadbalance, WeightedRoundRobinLoadBalancer,LeastConnectionLoadBalancer,RandomLoadBalancer,IPHashLoadBalancer. Actual logic implementation of all algorithm will be implemented in phase 3

---
+++ src/main/java/com/saba/loadbalancerlibrary/service/IPHashLoadBalancer.java
@@ -0,0 +1,14 @@
+package com.saba.loadbalancerlibrary.service;
+
+import com.saba.loadbalancerlibrary.dto.ServiceInstanceDto;
+
+import java.util.List;
+
+public class IPHashLoadBalancer implements LoadBalancer {
+
+    @Override
+    public ServiceInstanceDto selectInstance(List<ServiceInstanceDto> instances) {
+        // TODO: Phase 3 mein implement karenge
+        return null;
+    }
+}
---
+++ src/main/java/com/saba/loadbalancerlibrary/service/LeastConnectionsLoadBalancer.java
@@ -0,0 +1,14 @@
+package com.saba.loadbalancerlibrary.service;
+
+import com.saba.loadbalancerlibrary.dto.ServiceInstanceDto;
+
+import java.util.List;
+
+public class LeastConnectionsLoadBalancer implements LoadBalancer {
+
+    @Override
+    public ServiceInstanceDto selectInstance(List<ServiceInstanceDto> instances) {
+        // TODO: Phase 3 mein implement karenge
+        return null;
+    }
+}
